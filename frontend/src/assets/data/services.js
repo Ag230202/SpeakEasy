ffeeec50eaee522f21ed8365bfe363555b968006
Ag230202 <animeshgartia23@gmail.com>
2024-09-01 01:10:12 +0530

    Frontend Work Resub-Check

---
+++ frontend/src/assets/data/services.js
@@ -0,0 +1,38 @@
+export const services = [
+  {
+    name: "Articulation Therapy",
+    desc: "Addresses difficulties with pronouncing sounds, syllables or words correctly.",
+    bgColor: "rgba(254, 182, 13, .2)",
+    textColor: "#FEB60D",
+  },
+  {
+    name: "Fluency Therapy",
+    desc: "Helps individuals with stuttering or stammering to speak more smoothly and confidently.",
+    bgColor: "rgba(151, 113, 255, .2)",
+    textColor: "#9771FF",
+  },
+  {
+    name: "Language Therapy",
+    desc: "Enhances comprehension, expression and processing of language including reading and writing skills.",
+    bgColor: "rgba(1, 181, 197, .2)",
+    textColor: "#01B5C5",
+  },
+  {
+    name: "Cognitive-Communication Therapy",
+    desc: "Focuses on attention, memory, problem-solving and executive function skills.",
+    bgColor: "rgba(1, 181, 197, .2)",
+    textColor: "#01B5C5",
+  },
+  {
+    name: "Public speaking and Presentation skills",
+    desc: "Enhances confidence and effectiveness in public speaking situations.",
+    bgColor: "rgba(254, 182, 13, .2)",
+    textColor: "#FEB60D",
+  },
+  {
+    name: "Group Therapy",
+    desc: "Offers social skills, conversations and language practice in a group setting.",
+    bgColor: "rgba(151, 113, 255, .2)",
+    textColor: "#9771FF",
+  },
+];
