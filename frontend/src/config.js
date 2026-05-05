0436b82f755c207e5a7be771844e357fd672ca57
ANIMESH GARTIA <109869961+Ag230202@users.noreply.github.com>
2026-05-05 23:32:10 +0530

    Fix: Added /api/v1 prefix to BASE_URL

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,3 +1,3 @@
-export const BASE_URL='https://speakeasy-cdq3.onrender.com';
+export const BASE_URL='https://speakeasy-cdq3.onrender.com/api/v1';
 export const token=localStorage.getItem('token');
 
